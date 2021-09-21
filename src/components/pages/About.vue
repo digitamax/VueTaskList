81d2ce22cc4540834e57526e26bda7fcf798ceaf
Mamadou Yacine Gueye <momogueye7@gmail.com>
2021-09-21 22:03:35 +0000

    Updating About Page

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <h1>About the app</h1>
+        <h1>About the Vue TaskList App</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis suscipit, iure molestiae, aliquid iste inventore dolorum reprehenderit provident nobis, nulla consequuntur aspernatur corrupti sit debitis asperiores illo at corporis dicta?</p>
     </div>
 </template>
