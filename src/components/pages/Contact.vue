c118707bbd21ef5eceb71be3e4af26273afc122c
Mamadou Yacine Gueye <momogueye7@gmail.com>
2025-06-11 15:18:54 +0000

    Update Contact.vue
    Ajout d'un second numéro de téléphone

--- src/components/pages/Contact.vue
+++ src/components/pages/Contact.vue
@@ -2,6 +2,7 @@
     <div class="container">
         <h1>Contact Us</h1>
         <p>Tel: +221 78 417 58 18</p>
+        <p>Tel: +221 76 294 60 49</p>
         <p>Email: momogueye7@gmail.com</p>
     </div>
 </template>
