847f84f6fdccd8e4d0e6f9ba1c8ca4e937c1bed9
Mamadou Yacine Gueye <momogueye7@gmail.com>
2025-06-11 15:33:08 +0000

    Ajout d'un mail Pro sur la page de contact

--- src/components/pages/Contact.vue
+++ src/components/pages/Contact.vue
@@ -3,6 +3,7 @@
         <h1>Contact Us</h1>
         <p>Tel: +221 78 417 58 18</p>
         <p>Tel: +221 76 294 60 49</p>
+        <p>Email: contact@digitamax.com</p>
         <p>Email: momogueye7@gmail.com</p>
     </div>
 </template>
